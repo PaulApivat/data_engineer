d226f211c10d9088ee6660f8b803de03ae486f31
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-07-10 15:01:42 +0700

    update intermediate_python.py

--- practice/intermediate_python.py
+++ practice/intermediate_python.py
@@ -16,3 +16,34 @@ print(df.loc[[0]])
 
 between = np.logical_and(df['s_width'] > 3.2, df['s_width'] < 3.5)
 df[between]
+
+# Complete the code to return the output
+
+practice = {'python': 100, "r": 30, "sql": 10}
+
+for key, value in practice.items():
+    print(key + " practice has " + str(value) + " items")
+
+
+# Select code to return the out
+
+datacamp_data = {
+    'chapter': [1, 2, 3],
+    'course': ["Data Science", "Data Visualization", "Databases"],
+    'technology': ['Python', 'R', 'SQL']
+}
+
+datacamp_df = pd.DataFrame(datacamp_data)
+index = pd.Index(['P', 'R', 'S'])
+datacamp_df = datacamp_df.set_index([index])
+
+for lab, row in datacamp_df.iterrows():
+    print(lab)
+    print(row)
+
+# ----- What is the out put of this Numpy Array code 
+
+x = np.array([9, 5])
+y = np.array([16, 12])
+
+print(np.logical_or(x < 5, y > 15))
