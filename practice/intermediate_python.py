e973da58aa6f18e252c0f51c9739b9ce640afdde
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-07-12 09:13:39 +0700

    modified intermediate_python.py

--- practice/intermediate_python.py
+++ practice/intermediate_python.py
@@ -47,3 +47,25 @@ x = np.array([9, 5])
 y = np.array([16, 12])
 
 print(np.logical_or(x < 5, y > 15))
+
+# ------ Complete code output 
+
+numpy1 = np.array([17.2, 20.2, 8.25, 9.50])
+numpy2 = np.array([13.0, 24.0, 8.25, 9.0])
+
+# list slicing
+print(numpy1[::len(numpy1)-1])
+
+
+# ----- What is the output?
+datacamp_data = {
+    'chapter': [1, 2, 3],
+    'course': ["Data Science", "Data Visualization", "Databases"],
+    'technology': ['Python', 'R', 'SQL']
+}
+
+datacamp_df = pd.DataFrame(datacamp_data)
+index = pd.Index(['P', 'R', 'S'])
+datacamp_df = datacamp_df.set_index([index])
+
+print(datacamp_df.iloc[[1,2], [2]])
