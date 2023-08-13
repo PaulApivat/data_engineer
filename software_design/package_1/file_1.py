532377dd707d36e9270555101e8dd7a206cbaae4
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-08-13 20:12:11 +0700

    add software design directory for upcoming project

---
+++ software_design/package_1/file_1.py
@@ -0,0 +1,6 @@
+def file_1_function():
+    print("package_1.file_1_function")
+
+
+def file_1_function2():
+    print("package_1.file_1_function2")
--- software_design/temporary.py
+++ software_design/temporary.py
@@ -1,12 +1,21 @@
-
+from package_1.file_1 import file_1_function
+from package_1.file_1 import file_1_function2
 
 R = 10
 
-def some_function(x: int):
+
+def some_function(x: int) -> None:
     print(x)
+    print("original type-x ", type(x))
+
 
 def main():
-    some_function("int")
+    new_var = "int"
+    some_function(new_var)
+    print(type(new_var))
+    file_1_function()
+    file_1_function2()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
