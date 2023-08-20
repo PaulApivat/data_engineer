53eb64161ac08e0704c8b470db83d6c5d779afb1
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-08-20 20:56:24 +0700

    edit my_exercise for cohesion

--- software_design/7_principles/cohesion/my_exercise.py
+++ software_design/7_principles/cohesion/my_exercise.py
@@ -1,22 +1,21 @@
 import csv
 from typing import Any, List, Dict
-from dataclasses import dataclass, field
+
+"""
+dataclasses are not needed for this setup
+"""
 
 Record = dict[str, Any]
 
 
-@dataclass
-class DataTypes:
-    data_list: list[Record] = field(default_factory=list)
-    processed_data: list[Record] = field(default_factory=list)
-
-    def return_data(data: data_list):
-        return data_list
+INPUT_FILE = "data.csv"
+OUTPUT_FILE = "processed.csv"
+FIELD_NAMES_OUTPUT = ["name", "status", "is_active"]
 
 
 def read_data() -> List[Record]:
     data_list = []
-    with open("data.csv") as f:
+    with open(INPUT_FILE) as f:
         reader = csv.DictReader(f)
         for row in reader:
             data_list.append(row)
@@ -36,8 +35,8 @@ def process_data(data: List[Record]) -> List[Record]:
 
 
 def write_data(data: List[Record]) -> None:
-    with open("processed.csv", "w") as f:
-        writer = csv.DictWriter(f, fieldnames=["name", "status", "is_active"])
+    with open(OUTPUT_FILE, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=FIELD_NAMES_OUTPUT)
         writer.writeheader()
         writer.writerows(data)
 
