8bd8970c181ddfa3f0218ac4bd4a44a716fc7c66
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-09-20 18:16:05 +0700

    add data_quality_checks_gold.py and schema for eth_emissions_gold_schema

---
+++ practice/data-pipeline-project/scripts/data_quality_checks_gold.py
@@ -0,0 +1,41 @@
+import duckdb
+import json
+import jsonschema
+
+# Define the path to the schema file
+schema_path = "config/schema_definitions/eth_emissions_gold_schema.json"
+
+# Read the JSON schema from the file
+with open(schema_path, "r") as schema_file:
+    schema = json.load(schema_file)
+
+# Connect to the DuckDB database
+db_path = "data/gold/consumption_data.db"
+conn = duckdb.connect(db_path)
+
+# Define the table name
+table_name = "eth_emissions_gold"
+
+# Fetch data from the table
+query = f"SELECT * FROM {table_name}"
+df = conn.execute(query).fetchdf()
+
+# Convert DataFrame to a list of dictionaries (one per row)
+data = df.to_dict(orient="records")
+
+# Convert the 'datetime' column to string format
+for row in data:
+    row["datetime"] = str(row["datetime"])  # Convert Timestamp to string
+
+# Validate each row against the JSON schema
+for row in data:
+    try:
+        jsonschema.validate(instance=row, schema=schema)
+    except jsonschema.exceptions.ValidationError as e:
+        print(f"Validation error for row: {row}")
+        print(f"Error message: {e}")
+        # You can choose to handle or log the validation error here
+
+print("Validation successful.")
+# Close the database connection
+conn.close()
