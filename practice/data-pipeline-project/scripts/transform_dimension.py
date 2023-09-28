ce4e3afef8f53b22bd0684b3a7fa8e7b5eb49847
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-09-28 14:41:43 +0700

    update transform_dimension.py

--- practice/data-pipeline-project/scripts/transform_dimension.py
+++ practice/data-pipeline-project/scripts/transform_dimension.py
@@ -6,7 +6,7 @@ db_path = "data/bronze/user_bronze.db"
 conn = sqlite3.connect(db_path)
 
 # Extract unique values from the "blockchain" column in your Fact Table
-query = "SELECT DISTINCT blockchain FROM user_raw_2"
+query = "SELECT DISTINCT blockchain FROM user_raw_fact"
 unique_blockchains = conn.execute(query).fetchall()
 
 # Create a DataFrame with unique values
@@ -21,15 +21,15 @@ df_blockchains.to_sql("user_blockchain_dim", conn, if_exists="replace", index=Fa
 # Add a foreign key to your Fact Table
 # Replace "your_fact_table_name" with the actual name of your Fact Table
 query = f"""
-ALTER TABLE user_raw_2
+ALTER TABLE user_raw_fact
 ADD COLUMN blockchain_id INTEGER
 """
 conn.execute(query)
 
 # Add the foreign key constraint
 query = f"""
-UPDATE user_raw_2
-SET blockchain_id = (SELECT blockchain_id FROM user_blockchain_dim WHERE user_blockchain_dim.blockchain = user_raw_2.blockchain)
+UPDATE user_raw_fact
+SET blockchain_id = (SELECT blockchain_id FROM user_blockchain_dim WHERE user_blockchain_dim.blockchain = user_raw_fact.blockchain)
 """
 conn.execute(query)
 
@@ -38,5 +38,5 @@ conn.commit()
 conn.close()
 
 print(
-    "Dimension Table 'user_blockchain_dim' created and linked to the Fact Table: user_raw_2."
+    "Dimension Table 'user_blockchain_dim' created and linked to the Fact Table: user_raw_fact."
 )
