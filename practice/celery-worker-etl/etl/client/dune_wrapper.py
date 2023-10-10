053de86538c0d9872213618133baeb4ce7da4862
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-10-10 21:22:47 +0700

    add dune_wrapper parameterized

--- practice/celery-worker-etl/etl/client/dune_wrapper.py
+++ practice/celery-worker-etl/etl/client/dune_wrapper.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("DUNE_API_KEY")
+api_key = os.getenv("DUNE_API_KEY_TEAM")
 
 from dune_client.client import DuneClient
 from dune_client.query import QueryBase
@@ -19,7 +19,7 @@ class TestDuneAPI(unittest.TestCase):
         stream_handler = logging.StreamHandler(sys.stdout)
         logger.addHandler(stream_handler)
         query = QueryBase(
-            name="samaple eth_emissions", query_id=3088161
+            name="samaple t3_eth_fees_weekly_params", query_id=3091918
         )  # eth_emissions: 3032256 #eth_emissions_params: 3088161
         dune = DuneClient.from_env()
         results = dune.run_query(query)
---
+++ practice/celery-worker-etl/etl/client/dune_wrapper_3.py
@@ -0,0 +1,39 @@
+import os
+import logging
+from dotenv import load_dotenv
+from dotenv import load_dotenv
+from dune_client.types import QueryParameter
+from dune_client.client import DuneClient
+from dune_client.query import QueryBase
+
+
+load_dotenv()
+api_key = os.getenv("DUNE_API_KEY_TEAM")
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+
+
+def main():
+    # Define the query with parameters
+    query = QueryBase(
+        name="Sample Query",
+        query_id=3091918,
+        params=[
+            QueryParameter.date_type(name="1. start_date", value="2023-04-01 00:00:00"),
+            QueryParameter.date_type(name="2. end_date", value="2023-10-01 00:00:00"),
+            QueryParameter.number_type(name="offset", value=10),
+            QueryParameter.number_type(name="limit", value=10),
+        ],
+    )
+
+    print("Results available at", query.url())
+
+    # Create a DuneClient and run the query
+    dune = DuneClient.from_env()
+    results = dune.run_query(query)
+    logger.info(results)
+
+
+if __name__ == "__main__":
+    main()
