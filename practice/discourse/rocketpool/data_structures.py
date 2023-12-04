1aaa0e6e39c60ff94d3814b3495627659cd719ce
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-12-04 12:06:33 +0700

    add discourse/rocketpool folder with script to traverse forum data structure

---
+++ practice/discourse/rocketpool/data_structures.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def print_keys_at_level(data, level=1):
+    if isinstance(data, dict):
+        print(f"- Level {level}: {list(data.keys())}")
+        for value in data.values():
+            print_keys_at_level(value, level + 1)
+    elif isinstance(data, list):
+        for item in data:
+            print_keys_at_level(item, level + 1)
+
+
+url = (
+    "https://dao.rocketpool.net/t/about-the-liquid-staking-experience-category/210.json"
+)
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    pretty_json = json.dumps(
+        data, indent=4
+    )  # Convert the JSON data to a formatted string
+    print(pretty_json)
+    print("\n")
+    print("PRINT KEYS AT LEVELS")
+    print("\n")
+    print_keys_at_level(data)
+else:
+    print("Failed to retrieve data: HTTP Status Code", response.status_code)
