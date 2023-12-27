8680f97df07318ea146f8a5e06d8342f21123543
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-12-27 11:47:28 +0700

    implement exponential backoff for discourse pipeline

--- practice/discourse/rocketpool/pipeline/topic_post_pipeline.py
+++ practice/discourse/rocketpool/pipeline/topic_post_pipeline.py
@@ -15,6 +15,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import requests
 import datetime
 import logging
+import time
 
 # Configure logging
 logging.basicConfig(
@@ -54,10 +55,18 @@ class ProtocolTopicsPost(Base):
 # Function to fetch data from a paginated URL
 def fetch_paginated_data(base_url, start_page=0):
     page = start_page
+    delay = 1  # starting with 1 second delay
     while True:
         paginated_url = f"{base_url}?period=all&page={page}"
         logging.info(f"Fetching data from URL: {paginated_url}")
         response = requests.get(paginated_url)
+        if response.status_code == 429:
+            logging.warning(f"Rate limit reached, sleeping for {delay} seconds")
+            time.sleep(delay)
+            delay *= 2  # Double the delay for each retry
+            continue
+
+        delay = 1  # Reset delay after successful request
         if response.status_code != 200:
             logging.warning(
                 f"Failed to fetch data from {paginated_url}: Status code {response.status_code}"
@@ -87,6 +96,7 @@ def get_last_page_and_timestamp(session):
 
 # Main function to control the flow
 def main():
+    start_time = time.time()
     engine = create_engine("sqlite:///rocketpool.db")
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -95,7 +105,7 @@ def main():
     # Create a session for getting the last page and timestamp
     with Session() as temp_session:
         last_page, last_updated_at = get_last_page_and_timestamp(temp_session)
-    start_page = 0 if last_page is None else max(last_page - 1, 0)
+    start_page = 0 if last_page is None else max(last_page - 5, 0)
 
     new_posts_count = 0
     logging.info("Starting data fetching process")
@@ -168,6 +178,9 @@ def main():
         session.close()
         print(f"Total new posts added: {new_posts_count}")
         logging.info(f"Total new posts added: {new_posts_count}")
+    end_time = time.time()
+    total_time = end_time - start_time
+    logging.info(f"Total time taken for the pipeline: {total_time:.2f} seconds")
 
 
 # Execute the main function
